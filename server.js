7377845b1500795c5269172b0ed9e48f1e47d755
nickdvdev <nickdv.dev@gmail.com>
2021-03-20 21:12:50 +0200

    switch to mysql from psql

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const knex = require('knex');
 
 const PORT = process.env.PORT ?? 3000;
 
+// DATABASE
 const db = knex({
     client: 'pg',
     connection: {
@@ -17,9 +18,24 @@ const db = knex({
 
 const app = express();
 
+// MIDDLEWARE
 app.use(cors());
 app.use(bodyParser.json());
 
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('You have reached root page');
+});
+
+/*
+1. / -signin page - POST --success/fail
+2. /profile/:id - GET => user
+3. /register - POST --user
+
+*/
+
+// INIT
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
 });
